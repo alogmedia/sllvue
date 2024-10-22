3bc46a4dc69444b88e4cae2bee151a9321e20ac8
allo-mir <allo@mir-robots.com>
2024-10-22 11:54:53 +0200

    fix: browser support

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -19,7 +19,7 @@ import { Button } from "@/components/ui/button";
           <h1>
             Scandinavian
             <span
-              class="text-transparent bg-gradient-to-r from-[#facc15] to-primary bg-clip-text"
+              class="text-transparent bg-gradient-to-r from-[#facc15] to-primary bg-clip-text -webkit-background-clip-text"
               >Let Loose
             </span>
           </h1>
