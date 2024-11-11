8a93a95e305d40a665769556e4ee2bc02d89be0e
allo-mir <allo@mir-robots.com>
2024-11-11 12:45:03 +0100

    fix: privacy policy not working

---
+++ src/pages/PrivacyPolicy.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import Navbar from "../components/Navbar.vue";
+import Policy from "../components/Policy.vue";
+import Footer from "../components/Footer.vue";
+</script>
+
+<template>
+  <div class="video-container">
+    <video autoplay muted loop id="background-video">
+      <source src="/src/assets/bgmovie.mp4#t=4" type="video/mp4" />
+      Your browser does not support the video tag.
+    </video>
+  </div>
+  <Navbar />
+  <Policy />
+  <Footer />
+</template>
+
+<style>
+#background-video {
+  position: fixed;
+  background-color: #000;
+  right: 0;
+  bottom: 0;
+  min-width: 100%;
+  min-height: 100%;
+  opacity: 0.3;
+  z-index: -1;
+}
+
+@media only screen and (max-width: 600px) {
+  #background-video {
+    object-fit: cover;
+  }
+}
+</style>
